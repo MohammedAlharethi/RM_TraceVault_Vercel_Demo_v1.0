4b9e37596fe15d0ce7144dfe2055cd2bada0ec5e
MohammedAlharethi <79373211+MohammedAlharethi@users.noreply.github.com>
2026-08-02 07:26:20 +0300

    Add v4 CSS runtime loader

---
+++ runtime/css-loader.js
@@ -0,0 +1,1 @@
+(()=>{const s=document.createElement('style');s.id='tracevault-v4-styles';s.textContent=atob((window.__tv_css_chunks||[]).join(''));document.head.appendChild(s);delete window.__tv_css_chunks;})();
